d1a7da84ecb6713f962aabd1405140de70a0bc5f
RuochengShan <shanruocheng@163.com>
2020-03-17 19:23:53 -0400

    initialize project

---
+++ main.py
@@ -0,0 +1,21 @@
+'''
+main function
+1
+'''
+
+while True:
+
+    # 1. get games
+    #games = get_games()
+
+    # 2. get boards and moves
+    # for g in games:
+    #     board = get_boards(g)
+    #     move = get_moves(g)
+    #     # 3. do the alg
+    #     next_move = alg(board, move)
+    #     if next_move:
+    #         make_move(next_move)
+    #     else:
+    #         pass
+    pass
