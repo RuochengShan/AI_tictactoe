1037ddc76bcc4bce431fa5ecda230fff23025706
RuochengShan <shanruocheng@163.com>
2020-03-21 21:11:58 -0400

    main.py

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
-'''
-main function
-1
-'''
+from api.apis import get_board_map, get_board_string, get_games, get_moves, make_move
 
-while True:
+def main():
 
-    # 1. get games
-    #games = get_games()
+    while True:
 
-    # 2. get boards and moves
-    # for g in games:
-    #     board = get_boards(g)
-    #     move = get_moves(g)
-    #     # 3. do the alg
-    #     next_move = alg(board, move)
-    #     if next_move:
-    #         make_move(next_move)
-    #     else:
-    #         pass
-    pass
+        # 1. get games
+        #games = get_games()
+
+        # 2. get boards and moves
+        # for g in games:
+        #     board = get_boards(g)
+        #     move = get_moves(g)
+        #     # 3. do the alg
+        #     next_move = alg(board, move)
+        #     if next_move:
+        #         make_move(next_move)
+        #     else:
+        #         pass
+        pass
+
+if __name__ == '__main__':
+    main()
