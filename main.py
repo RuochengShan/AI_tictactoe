5a7c52ffc8e14a11cd7ebbd02dac4018c870f890
RuochengShan <shanruocheng@163.com>
2020-04-03 11:24:28 -0400

    main modify

--- main.py
+++ main.py
@@ -15,24 +15,27 @@ def main():
         game_ids = []
         for g in games:
             for g_id in g:
-                if "O" in g[g_id]:
+                if "O" in g[g_id] and g_id != "1135":
                     game_ids.append(g_id)
         # 2. get boards and moves
         for gid in game_ids:
             result = False
+            print("check game %s" % gid)
             move = get_moves(gid)
             chess_board = ChessBoard(12, 6)
             if move:
                 chess_board.import_from_moves(move)
-                player = chess_board.get_symbol(move)
                 x1, y1 = agent.findBestChess(chess_board.board, 2)
                 next_move = str(y1)+","+str(x1)
                 result = make_move(gid, next_move)
-            # else:
-            #     # try to move as first
-            #     next_move = mini_max_alg(chess_board, 1)
-            #     result = make_move(gid, next_move)
-
+                if result:
+                    move = get_moves(gid)
+                    chess_board.import_from_moves(move)
+                    print(chess_board.board)
+                    print("move for:", gid)
+                    print("move", move)
+                else:
+                    print("not your turn or game ended")
         time.sleep(1)
 
 
